33184066e5bc624f92c145b89b51f7129039ddc3
ZhitaoGan <owengan@bu.edu>
2025-06-27 15:31:28 +0800

    basic setup

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb  import MySQL
+
+app = Flask(__name__)
+
+app.secret_key = 'your secret key'
+
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'expense_project'
+
+# Intialize MySQL
+mysql = MySQL(app)
+
+# Routes
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
